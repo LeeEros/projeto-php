ad1c72a1750fda0598d3b3db7ff8dc9b0bb6277a
Kayane  Alebrante <alebrante.kayane22@gmail.com>
2024-11-23 19:45:42 -0300

    create controller cliente

--- controller/cliente.php
+++ controller/cliente.php
@@ -1,16 +1,65 @@
 <?php
 include("../model/banco.php");
+include("../model/model_cliente.php");
 include("../view/template.php");
 
-switch(@$_REQUEST["page"]){
+switch (@$_REQUEST["page"]) {
     case "novo":
         include("../view/cadastrar_cliente.php");
         break;
+
     case "listar":
         include("../view/listar_cliente.php");
         break;
-    default:
-        include ("../view/template.php");
+
+    case "visualizar":
+        $id = intval($_REQUEST['id']);
+        $cliente = getCliente($conexao, $id); 
+        include("../view/visualizar_cliente.php");
         break;
-    }
+
+    case "salvar":
+        $cpf = $_POST['cpf'];
+        $nome = $_POST['nome'];
+        $telefone = $_POST['telefone'];
+        $score = $_POST['score'];
+        $data_nascimento = $_POST['data_nascimento'];
+        $limite_credito = $_POST['limite_credito'];
+        $email = $_POST['email'];
+        $recebe_whatsapp = isset($_POST['recebe_whatsapp']) ? 1 : 0;
+        $recebe_email = isset($_POST['recebe_email']) ? 1 : 0;
+        $recebe_sms = isset($_POST['recebe_sms']) ? 1 : 0;
+
+    
+        $id = isset($_POST['id']) && !empty($_POST['id']) ? intval($_POST['id']) : null;
+
+
+        if (salvarCliente($conexao, $cpf, $nome, $telefone, $score, $data_nascimento, $limite_credito, $email, $recebe_whatsapp, $recebe_email, $recebe_sms, $id)) {
+            echo "<script>alert('Cliente " . ($id ? "atualizado" : "cadastrado") . " com sucesso!');</script>";
+        } else {
+            echo "<script>alert('Não foi possível " . ($id ? "atualizar" : "cadastrar") . " o cliente.');</script>";
+        }
+        echo "<script>location.href='?page=listar';</script>";
+        break;
+
+    case "editar":
+        $id = intval($_REQUEST['id']);
+        $cliente = getCliente($conexao, $id); 
+        include("../view/editar_cliente.php");
+        break;
+
+    case "excluir":
+        $id = intval($_REQUEST['id']);
+        if (excluirCliente($conexao, $id)) {
+            echo "<script>alert('Cliente excluído com sucesso!');</script>";
+        } else {
+            echo "<script>alert('Não foi possível excluir o cliente.');</script>";
+        }
+        echo "<script>location.href='?page=listar';</script>";
+        break;
+
+    default:
+        include("../view/cadastrar_cliente.php"); 
+        break;
+}
 ?>
--- view/cadastrar_cliente.php
+++ view/cadastrar_cliente.php
@@ -37,7 +37,77 @@
             </div>
             <div class="form-check">
                 <input class="form-check-input" type="radio" name="nao" id="nao" checked>
-                <label class="form-check-label" for="nao">Não</label>
+                <div class="container mt-5">
+    <h2>Cadastro de Cliente</h2>
+    <form action="?page=salvar" method="POST">
+        <div class="form-group">
+            <label for="cpf">CPF</label>
+            <input type="text" class="form-control" id="cpf" name="cpf" placeholder="" required>
+        </div>
+        <div class="form-group">
+            <label for="nome">Nome</label>
+            <input type="text" class="form-control" id="nome" name="nome" placeholder="Digite seu nome" required>
+        </div>
+        <div class="form-group">
+            <label for="telefone">Telefone</label>
+            <input type="text" class="form-control" id="telefone" name="telefone" placeholder="(42) 0 0000-0000" required>
+        </div>
+        <div class="form-group">
+            <label for="score">Score</label>
+            <input type="number" class="form-control" id="score" name="score" required>
+        </div>
+        <div class="form-group">
+            <label for="data_nascimento">Data de Nascimento</label>
+            <input type="date" class="form-control" id="data_nascimento" name="data_nascimento" required>
+        </div>
+        <div class="form-group">
+            <label for="limite_credito">Limite de Credito</label>
+            <input type="number" class="form-control" id="limite_credito" name="limite_credito" required>
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email" name="email" placeholder="Digite seu email" required>
+        </div>
+        
+        <div class="mb-3">
+            <label class="form-label">Recebe WhatsApp?</label>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="recebe_whatsapp" id="sim_whats" value="1">
+                <label class="form-check-label" for="sim_whats">Sim</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="recebe_whatsapp" id="nao_whats" value="0" checked>
+                <label class="form-check-label" for="nao_whats">Não</label>
+            </div>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Recebe Email?</label>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="recebe_email" id="recebe_email_sim" value="1">
+                <label class="form-check-label" for="recebe_email_sim">Sim</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="recebe_email" id="recebe_email_nao" value="0" checked>
+                <label class="form-check-label" for="recebe_email_nao">Não</label>
+            </div>
+        </div>
+
+        <div class="mb-3">
+            <label class="form-label">Recebe SMS?</label>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="recebe_sms" id="recebe_sms_sim" value="1">
+                <label class="form-check-label" for="recebe_sms_sim">Sim</label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="recebe_sms" id="recebe_sms_nao" value="0" checked>
+                <label class="form-check-label" for="recebe_sms_nao">Não</label>
+            </div>
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+</div>      <label class="form-check-label" for="nao">Não</label>
             </div>
         </div>
         <div class="form-group">
