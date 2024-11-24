6954ccb3d676d876025f1493cd9b8b9f037303de
Lee Eros Galaad Dias De Moura <95001466+LeeEros@users.noreply.github.com>
2024-11-24 02:03:22 -0300

    Update model_cliente.php

--- model/model_cliente.php
+++ model/model_cliente.php
@@ -1,7 +1,23 @@
 <?php
+
+function excluirVendasRelacionadas($conexao, $id_cliente) {
+    $sql = "DELETE FROM venda WHERE id_cliente = ?";
+    $stmt = $conexao->prepare($sql);
+    $stmt->bind_param("i", $id_cliente);
+    return $stmt->execute();
+}
+
+function excluirCliente($conexao, $id_cliente) {
+    excluirVendasRelacionadas($conexao, $id_cliente);
+    $sql = "DELETE FROM cliente WHERE id_cliente = ?";
+    $stmt = $conexao->prepare($sql);
+    $stmt->bind_param("i", $id_cliente);
+    return $stmt->execute();
+}
+
 function salvarCliente($conexao, $cpf, $nome, $telefone, $score, $data_nascimento, $limite_credito, $email, $recebe_whatsapp, $recebe_email, $recebe_sms, $id = null) {
-    if ($id_cliente) {
-        $sql = "UPDATE cliente SET cpf = ?, nome = ?, telefone = ?, score = ?, data_nascimento = ?, limite_credito = ?, email = ?, recebe_whatsapp = ?, recebe_email = ?, recebe_sms = ? WHERE id = ?";
+    if ($id) {
+        $sql = "UPDATE cliente SET cpf = ?, nome = ?, telefone = ?, score = ?, data_nascimento = ?, limite_credito = ?, email = ?, recebe_whatsapp = ?, recebe_email = ?, recebe_sms = ? WHERE id_cliente = ?";
         $stmt = $conexao->prepare($sql);
         $stmt->bind_param("ssssssssssi", $cpf, $nome, $telefone, $score, $data_nascimento, $limite_credito, $email, $recebe_whatsapp, $recebe_email, $recebe_sms, $id);
     } else {
@@ -12,20 +28,13 @@ function salvarCliente($conexao, $cpf, $nome, $telefone, $score, $data_nasciment
     return $stmt->execute();
 }
 
-function excluirCliente($conexao, $id_cliente) {
-    $sql = "DELETE FROM cliente WHERE id = ?";
-    $stmt = $conexao->prepare($sql);
-    $stmt->bind_param("i", $id_cliente);
-    return $stmt->execute();
-}
-
 function getCliente($conexao, $id_cliente) {
-    $sql = "SELECT * FROM cliente WHERE id = ?";
+    $sql = "SELECT * FROM cliente WHERE id_cliente = ?";
     $stmt = $conexao->prepare($sql);
-    $stmt->bind_param("i", $id);
+    $stmt->bind_param('i', $id_cliente);
     $stmt->execute();
-    $resultado = $stmt->get_result();
-    return $resultado->fetch_assoc();
+    $result = $stmt->get_result();
+    return $result->fetch_assoc(); 
 }
 
 function listarClientes($conexao) {
@@ -40,12 +49,10 @@ function listarClientes($conexao) {
 
 function getHistoricoCompras($conexao, $id_cliente) {
     $sql = "SELECT * FROM venda WHERE id_cliente = ?";
-        
     $stmt = $conexao->prepare($sql);    
     if ($stmt === false) {
         die('Erro na preparação da query: ' . $conexao->error);
     }
-        
     $stmt->bind_param("i", $id_cliente);
     $stmt->execute();
     
